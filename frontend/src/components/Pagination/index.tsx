8d38fff5b000f849a384175252e9e4805f5ab0bc
Adolfinhof <adolfofjunior@gmail.com>
2022-01-15 10:12:58 -0300

    Correção de erro do Heroku

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -16,13 +16,13 @@ function Pagination({ page, onChange }: Props) {
                 <button className="dsmovie-pagination-button"
                     disabled={page.first}
                     onClick={() => onChange(page.number - 1)}>
-                    <Arrow /> ''
+                    <Arrow />= 
                 </button>
                 <p>{`${page.number + 1} de ${page.totalPages}`}</p>
                 <button className="dsmovie-pagination-button"
                     disabled={page.last}
                     onClick={() => onChange(page.number + 1)}>
-                    <Arrow className="dsmovie-flip-horizontal" />''
+                    =<Arrow className="dsmovie-flip-horizontal" />
                 </button>
             </div>
         </div>
